e1574296bdd9eaf9bb128bc51dce27af41924404
kunalphalswal <111356016+kunalphalswal@users.noreply.github.com>
2024-08-19 23:11:44 +0800

    classification transformer complete

--- Transformers/Classify.py
+++ Transformers/Classify.py
@@ -6,54 +6,69 @@ from transformers import AutoTokenizer
 import torch.nn.functional as F
 import pandas as pd
 
-def handle_data(file_path,embedding_dim = 768,batch_size=10,seq_length=512):
-    # Load the IMDB dataset
-        dataset = pd.read_csv(file_path)
-        #TODO: use the full data
-        dataset = dataset.sample(frac=0.01).reset_index(drop=True)
-        #making the length of df divisible by 10 so that suitable batches can be made
-        while(len(dataset)%100!=0):
-            dataset=dataset[:-1]
-        reviews = dataset['review'].astype(str).tolist()
-        label_mapping = {
-            'positive': 1,
-            'negative': 0,
-            'neutral': 2
-        }
+def handle_data(file_path,batch_size=10,seq_length=512):
+      dataset = pd.read_csv(file_path)
+      dataset = dataset.sample(frac=1).reset_index(drop=True)
+      #making the length of df divisible by 10 so that suitable batches can be made
+      while(len(dataset)%100!=0):
+          dataset=dataset[:-1]
+      reviews = dataset['review'].astype(str).tolist()
+      label_mapping = {
+          'positive': 1,
+          'negative': 0,
+          'neutral': 2
+      }
 
-        # Map the sentiment labels to numeric values
-        dataset['sentiment'] = dataset['sentiment'].map(label_mapping)
-        labels = torch.tensor(dataset['sentiment'])
-        #Convert into vectors
-        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        vocab_size = tokenizer.vocab_size
-        # print(reviews.dtype)
-        encoded_reviews = tokenizer(
-            reviews,
-            return_tensors="pt", #retuning tensors are compatible with pytorch
-            padding=True,
-            truncation=True,
-            max_length=embedding_dim
-        )
-        # Define the split ratio (e.g., 80% train, 20% test)
-        train_size = int(0.8 * len(dataset))
-        test_size = len(dataset) - train_size
-        #divide data into train, test and then batches
-        batch_size=10
-        seq_length=512
-        train_reviews =encoded_reviews['input_ids'][:train_size].view(train_size//batch_size,batch_size,embedding_dim)
-        test_reviews =encoded_reviews['input_ids'][train_size:].view(test_size//batch_size,batch_size,embedding_dim)
-        train_labels = labels[:train_size].view(train_size//batch_size,batch_size)
-        test_labels = labels[train_size:].view(test_size//batch_size,batch_size)
-        return train_reviews,test_reviews,train_labels,test_labels,vocab_size
+      # Map the sentiment labels to numeric values
+      dataset['sentiment'] = dataset['sentiment'].map(label_mapping)
+      labels = torch.tensor(dataset['sentiment'])
+      #Convert into vectors
+      tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+      vocab_size = tokenizer.vocab_size
+      encoded_reviews = tokenizer(
+          reviews,
+          return_tensors="pt", #retuning tensors are compatible with pytorch
+          padding=True,
+          truncation=True,
+          max_length=seq_length
+      )
+      # Define the split ratio (e.g., 80% train, 20% test)
+      train_size = int(0.5 * len(dataset))
+      test_size = len(dataset) - train_size
+      #divide data into train, test and then batches
+      train_reviews =encoded_reviews['input_ids'][:train_size].view(train_size//batch_size,batch_size,seq_length)
+      test_reviews =encoded_reviews['input_ids'][train_size:].view(test_size//batch_size,batch_size,seq_length)
+      train_labels = labels[:train_size].view(train_size//batch_size,batch_size)
+      test_labels = labels[train_size:].view(test_size//batch_size,batch_size)
+      return (train_reviews.to(device), test_reviews.to(device),
+        train_labels.to(device), test_labels.to(device),
+        vocab_size)
+def train(model,x_train,x_labels,epochs=20,learning_rate=0.0001,lr_warmup=10000):
+  model.train(True)
+  opt = torch.optim.Adam(lr=learning_rate, params=model.parameters())
+  sch = torch.optim.lr_scheduler.LambdaLR(opt, lambda i: min(i / (lr_warmup / x_train.shape[1]), 1.0))
+  for epoch in range(epochs):
+    for batch_reviews,batch_labels in zip(x_train,x_labels):
+      opt.zero_grad()
+      out = model.forward(batch_reviews)
+      loss = F.nll_loss(out,batch_labels)
+      #log the loss
+      print(f"Epoch {epoch}, loss: {loss}")
+      loss.backward()
+      #update parameters
+      opt.step()
+      sch.step()
 
-def train(model,x_train,x_labels,epochs):
-    for epoch in epochs:
-        out = model.forward(x_train)
-        loss = F.nll_loss(out,x_labels)
-        loss.backward()
-        #update the parameters
-        #log the loss
+def test(model,x_test,x_labels):
+    model.eval(True)
+    correct = 0
+    for batch_reviews,batch_labels in zip(x_test,x_labels):
+        out = model.forward(batch_reviews)
+        for i,sentence in enumerate(out):
+            if(torch.argmax(out[i])==batch_labels[i]):
+                correct+=1
+    print(f"Accuracy: {(correct/(x_test.shape[0]*x_test.shape[1]))*100}%")
+
 if __name__=="__main__":
     # Get cpu, gpu or mps device for training.
     device = (
@@ -64,6 +79,12 @@ if __name__=="__main__":
         else "cpu"
     )
     print(f"Using {device} device")
+    #handle data
+    file_path="helper/IMDB.csv"
+    embedding_dim = 128
+    batch_size=5
+    seq_length=256
+    train_reviews,test_reviews,train_labels,test_labels,vocab_size=handle_data(file_path,batch_size=10,seq_length=512)
     if(os.path.exists("IMDBSentiment.pth")==False):
         #train and save the model
         """
@@ -76,20 +97,13 @@ if __name__=="__main__":
         5. Perform Training
         6. Perform Testing
         """
-        #handle data
-        file_path="helper/IMDB.csv"
-        embedding_dim = 768
-        batch_size=10
-        seq_length=512
-        train_reviews,test_reviews,train_labels,test_labels,vocab_size=handle_data(file_path,embedding_dim=768,batch_size=10,seq_length=512)
-
         #define the model 
-        model = CTransformer(k=embedding_dim,heads=8,depth=6,seq_length=seq_length,num_tokens=vocab_size,num_classes=2)
-        
+        model = CTransformer(k=embedding_dim,heads=8,depth=6,seq_length=seq_length,num_tokens=vocab_size,num_classes=2).to(device)
         #start the training
-
-
+        train(model,x_train=train_reviews,x_labels=train_labels,epochs=80,learning_rate=0.0001,lr_warmup=10000)
+        torch.save(model.state_dict(), "IMDBSentiment.pth")
     else:
         #load the saved model
         model = CTransformer().to(device)
-        model.load_state_dict(torch.load("IMDBSentiment.pth"))
+        model.load_state_dict(torch.load("IMDBSentiment.pth"),map_location=device)
+    test(model,test_reviews,test_labels)
--- Transformers/helper/transformers.py
+++ Transformers/helper/transformers.py
@@ -7,34 +7,51 @@ class SelfAttention(nn.Module):
         super().__init__()
         assert k%heads==0
         self.k,self.heads=k,heads
-        self.toKeys = nn.Linear(k,k,bias=False) 
-        self.toQueries = nn.Linear(k,k,bias=False)
-        self.toValues = nn.Linear(k,k,bias=False) 
+        #The linear function creates a transformation layer which returns a transformed output for an input. y = x(W.T)
+        self.toKeys = nn.Linear(k,k,bias=False) #wk
+        self.toQueries = nn.Linear(k,k,bias=False) #wq
+        self.toValues = nn.Linear(k,k,bias=False) #wv
+        #to concatenate the resultant chunks of each attention head
         self.unifyHeads = nn.Linear(k,k)
     def forward(self,x):
+        #the input would be a 3-d vector of form (batch_size,seq_len,in_features) because the sequence of input vectors comes in batches.
         b,t,k=x.size()
         h=self.heads
+        #but how does the matrix multiplication work on 3d matrix such as x: it also gives out a 3d matrix of form (batch_size,seq_len,out_features)
+        #here, both in and out features are k
         queries=self.toQueries(x)
         keys = self.toKeys(x)
         values = self.toValues(x)
 
         headSize = self.k//self.heads
 
+        #This simply reshapes the tensors to break the last dimension into two dimensions.
+        #purpose: to divide the features of each input vector into h parts, so that each head receives one chunk of that input vector.
+        #the chunks are of low dimensions and easier to compute individually.
         keys = keys.view(b,t,h,headSize)
         queries = queries.view(b,t,h,headSize)
         values = values.view(b,t,h,headSize)
 
+        # - fold heads into the batch dimension=> needed to compute the dot product parallely
         keys = keys.transpose(1, 2).contiguous().view(b * h, t, headSize)
         queries = queries.transpose(1, 2).contiguous().view(b * h, t, headSize)
         values = values.transpose(1, 2).contiguous().view(b * h, t, headSize)
-        
+        # now the first dimension is size of the batch: each batch has sequences of mini-vectors(one vector for each head).
+        # size of sequence is the block size.
+        # for each sequence, we have corresponding outputs
+        # how to concatenate those outputs once you have transformed the matrix? we un-transform it first before concatenating.
+
+        #compute weights
         raw_weights = torch.bmm(queries,keys.transpose(1,2))
-        
+        #raw_weights is of dimension: b*h, t, t
+
         raw_weights /= headSize**(1/2)
         weights = F.softmax(raw_weights,dim=2)
 
+        #apply self-attention to the input vectors
         out = torch.bmm(weights, values).view(b,h,t,headSize)
-        out = out.transpose(1,2).view(b,t,h*headSize)
+        out = out.transpose(1,2).contiguous().view(b,t,h*headSize)
+        #unifyHeads is not really necessary once we do h*headSize
         return self.unifyHeads(out)
 
 class TransformerBlock(nn.Module):
@@ -51,6 +68,7 @@ class TransformerBlock(nn.Module):
         nn.ReLU(),
         nn.Linear(4*k,k)
     )
+    #but sequential's units will output a number, not a vector? yep and a vector is k numbers
   def forward(self,x):
     attended = self.attention(x)
     x=self.norm1(attended+x)
@@ -58,33 +76,56 @@ class TransformerBlock(nn.Module):
     fedForward = self.ff(x)
     return self.norm2(fedForward+x)
 
+
 class CTransformer(nn.Module):
   def __init__(self,k,heads,depth,seq_length,num_tokens,num_classes):
     super().__init__()
 
-    self.num_tokens=num_tokens
-    #needs an input of maximum seq_length elements and vocab_size as num_tokens
-    self.token_emb = nn.Embedding(num_tokens,k) 
-    self.pos_emb = nn.Embedding(seq_length,k)
+    #layer for handling input
+    self.num_tokens=num_tokens #size of vocabulary i.e. no of unique tokens that the transformer knows.
+    self.token_emb = nn.Embedding(num_tokens,k) # map each token(integer) to a size k vector.
+    self.pos_emb = nn.Embedding(seq_length,k) # map each position (0->seq_length-1) to a size k vector
+    #these embedding layers will be initialized randomly, but trained with the input.
 
+    #transformer blocks
     tblocks=[]
     for i in range(depth):
       tblocks.append(TransformerBlock(k,heads))
     self.tblocks=nn.Sequential(*tblocks)
 
+    #layer for handling output: project to an array of size num_classes
     self.toProbs = nn.Linear(k,num_classes)
 
   def forward(self,x):
+    # process the input before feeding to transformer blocks
+    """
+        :param x: A (b, t) tensor of integer values representing
+                  words (in some predetermined vocabulary).
+                  Each batch has t tokens. so each batch corresponds to 1 sequence?
+        :return: A (b, c) tensor of log-probabilities over the
+                 classes (where c is the nr. of classes).
+                 Probability distribution over c classes for each batch
+    """
     tokens = self.token_emb(x)
     b, t, k = tokens.size()
-    positions = torch.arange(t)
+    positions = torch.arange(t,device=x.device)
     positions = self.pos_emb(positions)[None,:,:].expand(b,t,k)
-
+    '''
+    [None,:,:]
+    This adds a new dimension at the beginning of the tensor.
+    It changes the shape from (t, k) to (1, t, k).
+    expand(b,t,k)
+    This expands the tensor to shape (b, t, k), where b is the batch size.
+    It repeats the positional embeddings b times along the first dimension.
+    This operation doesn't allocate new memory; it creates a view of the original tensor.
+    '''
     x = tokens+positions
 
     x = self.tblocks(x)
 
-    x = x.mean(dim=1) 
-    x = self.toProbs(x) 
-    return F.log_softmax(x,dim=1) 
+    #process the output before returning
+    x = x.mean(dim=1) # calculates mean over the second dimension ie t. Now x is of shape (b,k) because each batch only has one vector
+    x = self.toProbs(x) # projects x to a shape (b,1,num_classes)
+    return F.log_softmax(x,dim=1) # calculates log of softmax across the second dimension ie num_classes. log is easier to handle than actual probability calculation
+    #done according to the task of the transformer.
 
